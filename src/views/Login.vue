7691c9fb5b915e6c56688b7d28fe99fc231c0f5d
Romias <844793587@qq.com>
2021-12-08 17:49:01 +0800

    project init v0.0.1

---
+++ src/views/Login.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="login">
+        <h1>This is {{ msg || 'undefined' }} page</h1>
+    </div>
+</template>
+<script>
+    export default {
+        name: 'Login',
+        data() {
+            return {
+                msg: 'Login'
+            }
+        }
+    }
+</script>
